2052f21915933b32fb326af32da50f513ea341c4
lucapiccinelli <luca.picci@gmail.com>
2013-07-26 00:25:35 +0200

    tracking
    tentativo di tracking dei barcode utilizzando le posizioni relative gli
    uni degli altri

--- barcode/barcode_video.py
+++ barcode/barcode_video.py
@@ -9,6 +9,7 @@ import sys
 import numpy as np    
 from BarcodeTools import BarcodeTools, BarcodeInfo
 import winsound
+from barcode.BarcodeTools import BarcodeFrame
 
 CV_CAP_PROP_FRAME_WIDTH = 3
 CV_CAP_PROP_FRAME_HEIGHT = 4
@@ -34,6 +35,8 @@ if __name__ == '__main__':
     frames_without_barcodes = 0
     image_decoded = False
     decode = True
+    
+    barcodes = []
     while True:
         x = y = 0
         extracted_bcodes = np.zeros((h, w), np.uint8)
@@ -54,63 +57,55 @@ if __name__ == '__main__':
             contours, bcode_imgs, shocked_bcode_imgs, center = barcodeTools.detect(frame)
             bcodes_num = len(contours)
             
-            if bcodes_num == 0:
-                frames_without_barcodes += 1
-            else:
-                frames_without_barcodes = 0
+            curr_frame = []
             
-            if frames_without_barcodes > 10: decode = True
+            barcodes_len = len(barcodes)
             
-            if frames_without_barcodes < 20 and decode:
-                for bcode_img, s_bcode_img, contour in zip(bcode_imgs, shocked_bcode_imgs, contours):
-                    box = np.int0(cv2.cv.BoxPoints(contour))
+            for bcode_img, s_bcode_img, contour in zip(bcode_imgs, shocked_bcode_imgs, contours):
+                decoded1 = barcodeTools.decode(bcode_img)
+                decoded2 = barcodeTools.decode(s_bcode_img)
+                decoded = decoded2 if decoded2 != '' else decoded1
+                
+                #calcolo le feature del barcode corrente
+                barcode_frame = BarcodeFrame(contour[0][0], contour[0][1], contour[3], bcode_img, decoded, contour)
+                for c in contours:
+                    if c != contour:
+                        barcode_frame.add_context_point(c[0][0], c[0][1], c[3])
+                curr_frame.append(barcode_frame)
+                
+                if barcodes_len == 0:
+                    barcodes.append(BarcodeInfo(bcode_img, decoded, barcode_frame, contour)) 
+                
+                                
+            #eseguo il il match col frame precedente e aggiorno la lista dei barcode
+            if barcodes_len > 0:
+                for b in curr_frame:
+                    prev_match = min(((b.match_score(prev_b.barcode_frame), prev_b)) for prev_b in barcodes)
+                    prev_match[1].add_update_candidate((prev_match[0], b))
+                    
+                for barcode in barcodes: 
+                    new_barcodes = barcode.update()
+                    for new_barcode in new_barcodes: 
+                        barcodes.append(new_barcode.bcode_img, new_barcode.decode_str, new_barcode, new_barcode.contour) 
+                        
+                barcodes = [barcode for barcode in barcodes if not barcode.to_remove]
+                
+                # disegno il tutto
+                for barcode in barcodes: 
+                    box = np.int0(cv2.cv.BoxPoints(barcode.contour))
                     cv2.drawContours(frame, [box], 0, (0, 255, 0), thickness = 2)
                     
+                    bcode_img = barcode.bcode_img
                     if x + bcode_img.shape[1] > w:
                         x = 0
                         y += 100
-                    extracted_bcodes[y:y + bcode_img.shape[0], x:x + bcode_img.shape[1]] = bcode_img
-                    s_extracted_bcodes[y:y + bcode_img.shape[0], x:x + bcode_img.shape[1]] = s_bcode_img
-                    x += bcode_img.shape[1] + 10
-
-                    
-                    decoded1 = barcodeTools.decode(bcode_img)
-                    decoded2 = barcodeTools.decode(s_bcode_img)
-                    
-                    decoded = decoded2 if decoded2 != '' else decoded1
-                    if decoded != '':
-                        cv2.putText(frame, decoded1, (contour[0][0], contour[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), thickness=2)
-                        cv2.putText(frame, decoded2, (contour[0][0], contour[0][1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
-    
+                        extracted_bcodes[y:y + bcode_img.shape[0], x:x + bcode_img.shape[1]] = bcode_img
+                        s_extracted_bcodes[y:y + bcode_img.shape[0], x:x + bcode_img.shape[1]] = s_bcode_img
+                        x += bcode_img.shape[1] + 10
                         
-                        if decoded not in bcodes_info:
-                            bcodes_info[decoded] = BarcodeInfo(bcode_img, decoded)
-                           
-                info_len = len(bcodes_info) 
-                if bcodes_num == 0: 
-                    frames_with_same_len = 0
+                    if decoded != '':
+                        cv2.putText(frame, barcode.decoded, (barcode.contour[0][0], barcode.contour[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, barcode.color, thickness=2)
                     
-                if info_len == prev_info_len and info_len != 0 and info_len == bcodes_num:
-                    frames_with_same_len += 1
-                else:
-                    frames_with_same_len = 0
-                    if info_len > bcodes_num and bcodes_num != 0: 
-                        bcodes_info = {}
-                
-                cv2.putText(frame, 'bcodes num:' + str(bcodes_num), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
-                cv2.putText(frame, 'len:' + str(info_len), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
-                cv2.putText(frame, 'same len:' + str(frames_with_same_len), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
-                cv2.putText(frame, '0 len:' + str(frames_without_barcodes), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=1)
-                if frames_with_same_len == 5:
-                    for k,v in bcodes_info.iteritems():
-                        print k
-                    print '\n'
-                    winsound.Beep(2500, 300)
-                    #image_decoded = True
-                    frames_with_same_len = 0
-                    bcodes_info = {}
-                    decode = False
-        
         cv2.imshow("original", frame)
         cv2.imshow("barcodes", extracted_bcodes)
         cv2.imshow("shocked barcodes", s_extracted_bcodes)
